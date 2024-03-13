aab96fcd95b4b55d63f25336276250cfbe03c86f
SD191100 <shivamdurgude1@gmail.com>
2024-03-13 14:22:57 +0530

    added the client to cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://blog-devsh.vercel.app'
+}));
 
 app.use('/api/posts', postRouter);
 app.use('/api/tags', tagRouter);
