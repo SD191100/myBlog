ea8b0e3dcde97a89245cda15b0f68126057e99c1
SD191100 <shivamdurgude1@gmail.com>
2024-03-13 17:39:20 +0530

    added a loader in client

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Post from '../components/Post';
+import { infinity } from 'ldrs'
+infinity.register('my-precious')
 // import { Helmet } from 'react-helmet'
 
 const Home = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const [posts, setPosts] = useState([])
     useEffect(() => {
+        setIsLoading(true);
         const fetch = async () => {
             try {
                 const res = await axios.get('https://api-blog-devsh-shivams-projects-1021053b.vercel.app/api/posts')
                 setPosts(res.data);
+                setIsLoading(false);
             } catch (error) {
                 console.log('Error fetching posts:', error);
             }
@@ -17,12 +22,8 @@ const Home = () => {
         fetch();
     }, []);
 
-    return <div className='max-w-3xl mx-auto px-4 ' >
-        {/* <Helmet>
-            <title>d.sh | Home</title>
-            <meta name="description" content="A collection of guides and tutorials on web development and devops." />
-        </Helmet> */}
-        <div className="mt-5 mb-5 sm:mb-12 sm:mt-4">
+    return <div className='max-w-3xl mx-auto px-4 relative ' >
+        <div className="mt-5 mb-5 sm:mb-12 sm:mt-4 ">
             <h1 className="text-white font-bold mb-2 sm:mb-4 sm:text-9xl text-5xl">
                 dev.sh
             </h1>
@@ -30,6 +31,7 @@ const Home = () => {
                 A collection of guides and tutorials on web development and devops.
             </p>
         </div>
+        {isLoading ?<div className='absolute top-1/2 left-1/2' ><my-precious color="white"></my-precious></div> : null}
         {posts.map((post) => (
             <Post key={post._id} post={post} />
         ))}
